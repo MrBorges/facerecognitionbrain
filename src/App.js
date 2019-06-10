17904ea900dba9760c6b6566b93b68bbd53ec593
Lucas Borges <lucas.info@gmail.com>
2019-06-10 20:40:06 +0000

    Added box around faces.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,28 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log('width: ',width,'height: ',height);
+      return{
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+  }
+
+  displayFaceBox = (box) =>{
+    console.log(box);
+    this.setState({box: box});
 
   }
 
@@ -33,17 +54,9 @@ class App extends Component {
   onButtonSubmit = (event) =>{        
     this.setState( {imageUrl: this.state.input} )
     console.log('Clicked');
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-    function(response) {
-      // do something with response
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err) {
-      // there was an error
-      console.log('Something broke', err);
-
-    }
-    );
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log('Eeerr... something broke',err))
   }
 
   render (){
@@ -53,7 +66,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange = { this.onInputChange } onButtonSubmit = { this.onButtonSubmit } />
-        <FaceRecognition imageUrl= {this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl= {this.state.imageUrl}/>
       </div>
     );
   }
